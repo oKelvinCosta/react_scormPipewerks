2240f43efbb4e575649fe555817ea5ae52cbee6d
Kelvin Costa <okelvincosta@gmail.com>
2025-04-16 13:53:11 -0300

    Refactor: Remove unused SCORM script import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./App.jsx";
+// SCORM
 import ScormWrapper from "./ScormWrapper";
 import LoadWrapper from "./LoadWrapper";
-
-// Import SCORM scripts
-// import "./lib/scorm-toolkit-esm/index.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
